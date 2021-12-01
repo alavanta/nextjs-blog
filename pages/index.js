f4a1aef1ac4d2708b2ec0a78f7ed84eb29f75049
Dhieo Deva Alavanta <alavanta.deva@gmail.com>
2021-12-01 12:27:38 +0700

    first commit not get cherry picked

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Date from '../components/date'
 
 export default function Home ({ allPostsData }) {
+  //test cherry pick first commit
   return (
     <Layout home>
       <Head>
